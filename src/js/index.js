f005ab6cda66e2f9e23ebc27e12aea743e8d1b0d
lucaslira <lucasliraone@gmail.com>
2023-09-28 16:56:01 -0300

    code changed

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,16 @@
-import { Router } from './router.js';
 import { Controls } from './controls.js';
+import { Router } from './router.js';
 const controls = new Controls();
 const router = new Router();
 
-controls.run();
+// controls.run();
 
+router.add(404, '/pages/404.html');
 router.add('/', '/pages/home.html');
 router.add('/universe', '/pages/universe.html');
 router.add('/exploration', '/pages/exploration.html');
-router.add(404, '/pages/404.html');
 
 router.handle();
 
-window.onpopstate = () => router.handle();
 window.route = () => router.route();
+window.onpopstate = () => router.handle();
--- src/js/router.js
+++ src/js/router.js
@@ -7,18 +7,22 @@ export class Router{
 
     route(event){
         event = event || window.event;
-        event.preventDefault();  
+        event.preventDefault();
         
         window.history.pushState({}, "", event.target.href);
-    
+
         this.handle();
     }
     
     handle(){
         const { pathname } = window.location;
         const route = this.routes[pathname] || this.routes[404];
+        const page = pathname.replace('/', '');
+
         fetch(route)
         .then(data=> data.text())
         .then(html=> document.querySelector('#app').innerHTML = html);
+
+        document.querySelector(`#${page}`).classList.add('active');
     }
 }
