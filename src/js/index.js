ea8805ebdb0bde36064b57d585cffec9d6fa030b
lucaslira <lucasliraone@gmail.com>
2023-09-28 23:07:58 -0300

    router.js updated

--- src/js/index.js
+++ src/js/index.js
@@ -3,13 +3,12 @@ import { Router } from './router.js';
 const controls = new Controls();
 const router = new Router();
 
-// controls.run();
-
 router.add(404, '/pages/404.html');
 router.add('/', '/pages/home.html');
 router.add('/universe', '/pages/universe.html');
 router.add('/exploration', '/pages/exploration.html');
 
+controls.run();
 router.handle();
 
 window.route = () => router.route();
